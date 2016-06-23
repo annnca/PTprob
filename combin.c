fbc7d5a6a8c8d1839409ceef0144474a3a731f21
annnca <stanasel.anca@yahoo.com>
2016-06-23 16:42:33 +0300

    Update combin.c

--- combin.c
+++ combin.c
@@ -19,26 +19,21 @@ void printCombination(int arr[], int n, int r)
    start & end ---> Staring and Ending indexes in arr[]
    index  ---> Current index in data[]
    r ---> Size of a combination to be printed */
-void combinationUtil(int arr[], int data[], int start, int end,
-                     int index, int r)
-{
+void combinationUtil(int arr[], int data[], int start, int end,int index, int r){
     // Current combination is ready to be printed, print it
-    if (index == r)
-    {
+    if (index == r) {
         int j,i;
         for (j=0; j<r; j++)
             printf("%d ", data[j]);
         printf("\n");
         return;
     }
-
     // replace index with all possible elements. The condition
     // "end-i+1 >= r-index" makes sure that including one element
     // at index will make a combination with remaining elements
     // at remaining positions
     int i;
-    for (i=start; i<=end && end-i+1 >= r-index; i++)
-    {
+    for (i=start; i<=end && end-i+1 >= r-index; i++)  {
         data[index] = arr[i];
         combinationUtil(arr, data, i+1, end, index+1, r);
     }
