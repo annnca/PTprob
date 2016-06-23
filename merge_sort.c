baef6b9b4087ea2f5c217e50ff9829ac24f121ad
annnca <stanasel.anca@yahoo.com>
2016-06-23 21:29:20 +0300

    Update merge_sort.c

--- merge_sort.c
+++ merge_sort.c
@@ -1,60 +1,65 @@
-
+/* Merge sort in C */
 #include<stdio.h>
+#include<stdlib.h>
 
-void interclasare(int *x,int *v,  int sx, int fx, int sy, int fy)
-{
-    int t;
-	int i = sx, j = sy, k = 0;
-	while (i <= fx && j <= fy)
-	{
-		if (x[i] <= x[j])
-		{
-			v[++k] = x[i];
-			++i;
-		}
-		else
-		{
-			v[++k] = x[j];
-			++j;
-		}
+// Function to Merge Arrays L and R into A. 
+// lefCount = number of elements in L
+// rightCount = number of elements in R. 
+void Merge(int *A,int *L,int leftCount,int *R,int rightCount) {
+	int i,j,k;
+
+	// i - to mark the index of left aubarray (L)
+	// j - to mark the index of right sub-raay (R)
+	// k - to mark the index of merged subarray (A)
+	i = 0; j = 0; k =0;
+
+	while(i<leftCount && j< rightCount) {
+		if(L[i]  < R[j]) A[k++] = L[i++];
+		else A[k++] = R[j++];
 	}
-	if (i>fx)
-		for (t = j;t <= fy;++t)
-			v[++k] = x[t];
-	if (j>fy)
-		for (t = i;t <= fx;++t)
-			v[++k] = x[t];
-
-	for (i = 1;i <= k;i++)
-		x[sx + i - 1] = v[i];
-
+	while(i < leftCount) A[k++] = L[i++];
+	while(j < rightCount) A[k++] = R[j++];
 }
 
-void mergesort(int *x,int *v, int s, int f)
-{
-	if (s != f)
-	{
-		mergesort(x,v,  s, (s + f) / 2);
-		mergesort(x,v, (s + f) / 2 + 1, f);
-		interclasare(x,v, s, (s + f) / 2, (s + f) / 2 + 1, f);
-	}
-}
-void print(int *a, int n)
-{
-    int i;
-    for (i = 1; i < n; ++i)
-    {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
+// Recursive function to sort an array of integers. 
+void MergeSort(int *A,int n) {
+	int mid,i, *L, *R;
+	if(n < 2) return; // base condition. If the array has less than two element, do nothing. 
+
+	mid = n/2;  // find the mid index. 
+
+	// create left and right subarrays
+	// mid elements (from index 0 till mid-1) should be part of left sub-array 
+	// and (n-mid) elements (from mid to n-1) will be part of right sub-array
+	L = (int*)malloc(mid*sizeof(int)); 
+	R = (int*)malloc((n- mid)*sizeof(int)); 
+	
+	for(i = 0;i<mid;i++) L[i] = A[i]; // creating left subarray
+	for(i = mid;i<n;i++) R[i-mid] = A[i]; // creating right subarray
+
+	MergeSort(L,mid);  // sorting the left subarray
+	MergeSort(R,n-mid);  // sorting the right subarray
+	Merge(A,L,mid,R,n-mid);  // Merging L and R into A as sorted list.
+        free(L);
+        free(R);
 }
 
-int main()
-{
-    int v[50];
-    int a[] = { 5, 1, 8, 2, 7, 3, 9, 4, 6 };
-    print(a, 9);
-    mergesort(a, v,0,9);
-    print(v, 9);
-    return 0;
+int main() {
+	/* Code to test the MergeSort function. */
+	
+	int A[] = {6,2,3,1,9,10,15,13,12,17}; // creating an array of integers. 
+	int i,numberOfElements;
+
+	// finding number of elements in array as size of complete array in bytes divided by size of integer in bytes. 
+	// This won't work if array is passed to the function because array
+	// is always passed by reference through a pointer. So sizeOf function will give size of pointer and not the array.
+	// Watch this video to understand this concept - http://www.youtube.com/watch?v=CpjVucvAc3g  
+	numberOfElements = sizeof(A)/sizeof(A[0]); 
+
+	// Calling merge sort to sort the array. 
+	MergeSort(A,numberOfElements);
+
+	//printing all elements in the array once its sorted.
+	for(i = 0;i < numberOfElements;i++) printf("%d ",A[i]);
+	return 0;
 }
